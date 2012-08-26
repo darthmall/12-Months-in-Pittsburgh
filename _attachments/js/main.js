7cda2bfca77fd91cf69fb5863daef2119889e581
darthmall <wallace.sheehan@gmail.com>
2012-08-26 16:39:26 -0600

    Construct a sunburst diagram instead of the stacked bargraphs I'd been using

--- _attachments/js/main.js
+++ _attachments/js/main.js
@@ -4,76 +4,65 @@ CARDVIZ = {
 	],
 
 	init: function() {
-		var colors = d3.select('#colors').append('g');
+		var width = $('#colors').width(),
+			height = $('#colors').height(),
+			radius = Math.min(width, height) / 2;
 
-		var setColors = function(json) {
-			var w = $('#colors').width();
-			var h = $('#colors').height();
-			var center = [Math.floor(w/2), Math.floor(h/2)]
-			var scale = d3.scale.linear()
-				.domain([0, 6])
-				.range([100, 375])
+		var colors = d3.select('#colors').append('g')
+			.attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
 
-			if (json) {
-				var gradients = d3.select('#colors').selectAll('linearGradient')
-					.data(json.rows)
-					.enter()
-					.append('linearGradient')
-					.attr('id', function(d) {
-						return d.id + '-gradient';
+		var partition = d3.layout.partition()
+			.size([2 * Math.PI, radius * radius])
+			.sort(function (a, b) { return b.frequency - a.frequency; })
+			.children(function (d) { return (d.children) ? d.children : d.values; })
+			.value(function (d) { return d.frequency; });
+
+		var arc = d3.svg.arc()
+			.startAngle(function (d) { return d.x; })
+			.endAngle(function (d) { return d.x + d.dx; })
+			.innerRadius(function (d) { return Math.sqrt(d.y); })
+			.outerRadius(function (d) { return Math.sqrt(d.y + d.dy); });
+
+		var lScale = d3.scale.linear()
+			.domain([1, 3])
+			.range([0.9, 0.7]);
+
+		d3.json('/cards/_design/cardviz/_view/colors', function (json) {
+
+			var cards = d3.nest()
+				.key(function (d) { return d.key[2]; })
+				.rollup(function (d) { 
+					return d.map(function(v) {
+						return v.value;
 					});
+				});
 
-				gradients.selectAll('stop')
-					.data(function (d) {
-						return d.value;
-					})
-					.enter()
-					.append('stop')
-					.attr('offset', function (d) {
-						return d.offset;
-					})
-					.attr('stop-color', function (d) {
-						return '#' + d['stop-color'];
-					});
+			var months = d3.nest()
+				.key(function (d) { return d.key[1]; })
+				.rollup(function (d) { return cards.entries(d); });
 
+			var cardData = {
+				'key': 'cards',
+				'values': d3.nest()
+					.key(function (d) { return d.key[0]; })
+					.rollup(function (d) { return months.entries(d); })
+					.entries(json.rows)
+				};
 
-				colors.selectAll('rect')
-					.data(json.rows)
-					.enter()
-					.append('rect')
-					.attr('stroke', 'black')
-					.attr('fill', function (d) {
-						return 'url(#' + d.id + '-gradient)';
-				  	})
-				  	.attr('x', function (d, i) {
-				  		return scale(d.key[2]);
-				  	})
-				  	.attr('y', -20)
-				  	.attr('width', 40)
-				  	.attr('height', 40)
-				  	.attr('transform', function (d) {
-				  		var angle = 30 + d.key[1] * 30;
-				  		return 'rotate(' + angle + ')';
-				  	});
-
-				 colors.selectAll('text')
-				 	.data(json.rows)
-				 	.enter()
-				 	.append('text')
-				 	.attr('x', 0)
-				 	.attr('y', -75)
-				 	.attr('width', 50)
-				 	.attr('transform', function (d) {
-				 		var angle = 120 + (d.key[1] * 30);
-				 		return 'rotate(' + angle + ')';
-				 	})
-				 	.text(function (d) {
-				 		return CARDVIZ.MONTHS[d.key[1]];
-				 	});
-
-				 resize();
-			}
-		};
+			colors.data([cardData]).selectAll('path')
+				.data(partition.nodes)
+				.enter()
+				.append('path')
+				.attr('display', function (d) { return (d.depth) ? null : 'none'; })
+				.attr('d', arc)
+				.attr('fillRule', 'evenodd')
+				.attr('id', function (d) { return d.key + '-' + d.depth + '-' + d.values; })
+				.style('stroke', '#fff')
+				.style('strokeWidth', '3')
+				.style('fill', function (d) { 
+					return (d.color) ? '#' + d.color : d3.hsl(0, 0, lScale(d.depth)).toString();
+				});
+		});
 
 		var bargraph = function (json) {
 			if (json) {
@@ -119,10 +108,9 @@ CARDVIZ = {
 			var w = svg.width();
 			var h = svg.height();
 
-			colors.attr('transform', 'translate(' + w/2 + ',' + h/2 + ') scale(' + w/746 + ')');
+			colors.attr('transform', 'translate(' + w/2 + ',' + h/2 + ') scale(' + w/width + ')');
 		}
 
-		d3.json('/cards/_design/cardviz/_view/colors', setColors);
 		d3.json('/cards/_design/cardviz/_view/senders?group=true', bargraph);
 		$(window).resize(resize);
 	}
--- views/colors/map.js
+++ views/colors/map.js
@@ -3,26 +3,8 @@ function (doc) {
 	var year = Number(toks[0].substr(0, 4));
 	var month = Number(toks[0].substr(4,6)) - 1;
 	var num = Number(toks[1]) - 1;
-	var stops = [];
-	var offset = 0;
 
-	for (var i = 0; i < doc.colors.length; i++) {
-		if (Math.floor(doc.colors[i].frequency * 100) > 0) {
-			stops.push({
-				'stop-color': doc.colors[i].color,
-				'offset': offset,
-			});
-
-			offset += doc.colors[i].frequency;
-
-			if ((i + 1) < doc.colors.length) {
-				stops.push({
-					'stop-color': doc.colors[i].color,
-					'offset': offset
-				});
-			}
-		}
-	}
-
-	emit([year, month, num], stops);
+	for (var i = doc.colors.length - 1; i >= 0; i--) {
+		emit([year, month, num], doc.colors[i]);
+	};
 }
