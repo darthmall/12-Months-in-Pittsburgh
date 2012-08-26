f1cff693cb2e064553bba318e36b2538779cd54f
darthmall <wallace.sheehan@gmail.com>
2012-08-26 13:42:21 -0600

    Scale the chart with the size of the SVG canvas.

--- _attachments/js/main.js
+++ _attachments/js/main.js
@@ -4,10 +4,15 @@ CARDVIZ = {
 	],
 
 	init: function() {
+		var colors = d3.select('#colors').append('g');
+
 		var setColors = function(json) {
 			var w = $('#colors').width();
 			var h = $('#colors').height();
 			var center = [Math.floor(w/2), Math.floor(h/2)]
+			var scale = d3.scale.linear()
+				.domain([0, 6])
+				.range([100, 375])
 
 			if (json) {
 				var gradients = d3.select('#colors').selectAll('linearGradient')
@@ -31,33 +36,32 @@ CARDVIZ = {
 						return '#' + d['stop-color'];
 					});
 
-				var viz = d3.select('#colors').append('g')
-					.attr('transform', 'translate(' + center[0] + ',' + center[1] + ')');
 
-				viz.selectAll('rect')
+				colors.selectAll('rect')
 					.data(json.rows)
 					.enter()
 					.append('rect')
+					.attr('stroke', 'black')
 					.attr('fill', function (d) {
 						return 'url(#' + d.id + '-gradient)';
 				  	})
 				  	.attr('x', function (d, i) {
-				  		return 60 + (d.key[2] * 32);
+				  		return scale(d.key[2]);
 				  	})
-				  	.attr('y', -15)
-				  	.attr('width', 30)
-				  	.attr('height', 30)
+				  	.attr('y', -20)
+				  	.attr('width', 40)
+				  	.attr('height', 40)
 				  	.attr('transform', function (d) {
 				  		var angle = 30 + d.key[1] * 30;
 				  		return 'rotate(' + angle + ')';
 				  	});
 
-				 viz.selectAll('text')
+				 colors.selectAll('text')
 				 	.data(json.rows)
 				 	.enter()
 				 	.append('text')
 				 	.attr('x', 0)
-				 	.attr('y', -150)
+				 	.attr('y', -75)
 				 	.attr('width', 50)
 				 	.attr('transform', function (d) {
 				 		var angle = 120 + (d.key[1] * 30);
@@ -66,6 +70,8 @@ CARDVIZ = {
 				 	.text(function (d) {
 				 		return CARDVIZ.MONTHS[d.key[1]];
 				 	});
+
+				 resize();
 			}
 		};
 
@@ -108,8 +114,17 @@ CARDVIZ = {
 			}
 		};
 
+		var resize = function () {
+			var svg = $('#colors');
+			var w = svg.width();
+			var h = svg.height();
+
+			colors.attr('transform', 'translate(' + w/2 + ',' + h/2 + ') scale(' + w/746 + ')');
+		}
+
 		d3.json('/cards/_design/cardviz/_view/colors', setColors);
 		d3.json('/cards/_design/cardviz/_view/senders?group=true', bargraph);
+		$(window).resize(resize);
 	}
 };
 
